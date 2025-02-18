2abd3ceed1cf2be285e1b85408ebbdaf9fdb9e8b
Akash T K <akash.tk333@gmail.com>
2025-02-18 09:26:14 +0530

    Update layout.tsx

--- nextjs-portfolio/src/app/layout.tsx
+++ nextjs-portfolio/src/app/layout.tsx
@@ -114,6 +114,7 @@ export default function Layout({ children }: { children: ReactNode }) {
             width: '100%',
             height: '100%',
             zIndex: -1,
+            backgroundColor: '#03020d',
           }}
         />
 
